24ff2e9c325876510945ca1d7c1342ae8a0590d7
rayaan <rayaan.shahid.rs@gmail.com>
2019-05-21 11:10:11 -0700

    thread manager

--- src/main/java/com/rayaan/dupcloudparser/GCS/GCSFileReader.java
+++ src/main/java/com/rayaan/dupcloudparser/GCS/GCSFileReader.java
@@ -1,5 +1,6 @@
 package com.rayaan.dupcloudparser.GCS;
 
+import com.google.appengine.api.ThreadManager;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
@@ -7,11 +8,19 @@ import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 public class GCSFileReader {
 
    public void readBlob(String bucketName,String objectName){
-       BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-       BlobKey blobKey = blobstoreService.createGsBlobKey(
-               "/gs/" + bucketName + "/" + objectName);
-       byte[] Data=blobstoreService.fetchData(blobKey,1,10);
-       System.out.println(Data.toString());
-       //blobstoreService.serve(blobKey, resp);
+
+       Thread thread = ThreadManager.createBackgroundThread(new Runnable() {
+           @Override
+           public void run() {
+               BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+               BlobKey blobKey = blobstoreService.createGsBlobKey(
+                       "/gs/" + bucketName + "/" + objectName);
+               byte[] Data=blobstoreService.fetchData(blobKey,1,10);
+               System.out.println(Data.toString());
+               //blobstoreService.serve(blobKey, resp);
+           }
+       });
+
+       thread.start();
    }
 }
